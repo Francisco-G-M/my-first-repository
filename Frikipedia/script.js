889824c52ca0ff295076bb5948fb1fd114826668
Francisco-G-M <147457527+Francisco-G-M@users.noreply.github.com>
2023-11-15 13:31:18 -0300

    Add files via upload

--- Frikipedia/script.js
+++ Frikipedia/script.js
@@ -363,3 +363,13 @@ console.log("Consumible: " + compras[h]);
 console.log(compras[h]);
 }
 }
+var x = 16;
+do {
+  console.log(x);
+  x++;
+}while (x<10);
+x = 8;
+while (x<10) {
+  console.log(x);
+  x++;
+}
