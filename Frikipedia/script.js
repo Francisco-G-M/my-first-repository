3e6e54d2a578245a936e670c379894d4ab7b5264
Francisco-G-M <147457527+Francisco-G-M@users.noreply.github.com>
2023-12-02 18:53:17 -0300

    Add files via upload

--- Frikipedia/script.js
+++ Frikipedia/script.js
@@ -569,3 +569,17 @@ var b = 6;
 [b, a] = [a, b];
 console.log("a: " + a);
 console.log("b: " + b);
+var a;
+var b;
+var arr;
+[a, b, ...arr] = [1, 2, 3, 4, 5, 6, 7];
+console.log(a);
+console.log(b);
+console.log(arr);
+const arregloInicial = [1, 2, 3, 4, 5, 6, 7, 8];
+function removerTresPrimerosElementos(arreglo) {
+  const [,,, ...nuevoArreglo] = arreglo;
+  return nuevoArreglo;
+}
+const arregloFinal = removerTresPrimerosElementos(arregloInicial);
+console.log(arregloFinal);
