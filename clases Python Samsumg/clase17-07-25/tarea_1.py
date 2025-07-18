f06cac25d786eebd452ef921b9497453179d900a
Francisco-G-M <147457527+Francisco-G-M@users.noreply.github.com>
2025-07-17 22:41:50 -0300

    Delete clases Python Samsumg directory

--- clases Python Samsumg/clase17-07-25/tarea_1.py
+++
@@ -1,6 +0,0 @@
-monto_total = float(input("Ingrese el monto total: "))
-propina = float(input("Ingrese el porcentaje de propina que le gustaría dejar: "))
-monto_total_propina = (monto_total*propina)/100
-monto_total_cuenta = monto_total + monto_total_propina
-print(f"El monto total de la propina es: {monto_total_propina}")
-print(f"El monto total de la cuenta es: {monto_total_cuenta}")
--- clases Python Samsumg/clase17-07-25/tarea_2.py
+++
@@ -1,6 +0,0 @@
-nombre = [input("Ingrese su nombre: ")]
-apellido = [input("Ingrese su apellido: ")]
-año_nacimiento = [input("Ingrese su año de nacimiento: ")]
-nombre_usuario = []
-nombre_usuario.append(nombre[0])
-print(nombre_usuario)
